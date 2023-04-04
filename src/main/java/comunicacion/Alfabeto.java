d4661c2b71c596feee68f0ebb37a66dddc22c62b
DanielCaroD <124928694+DanielCaroD@users.noreply.github.com>
2023-04-04 18:28:24 -0500

    .

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,43 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma {
+	private static String[] letras;
+	private String interpretacion;
+	
+	public Alfabeto(String origen, String[] letras, String interpretacion) {
+		super(origen);
+		Alfabeto.letras = letras;
+		this.interpretacion = interpretacion;
+	}
+	
+	public String[] getLetras() {
+		return letras;
+	}
+	public void setLetras(String[] letras) {
+		Alfabeto.letras = letras;
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+	
+	public String interpretacion() {
+		return this.interpretacion;
+	}
+	
+	public String toString() {
+		String a = "";
+		for(int i=0; i<letras.length; i++) {
+			a = a + letras[i] + ", ";
+		}
+		return a;
+	}
+}
---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,38 @@
+package comunicacion;
+
+public class Fabula extends Escrito {
+	private String ensenanza;
+	private String interpretacion;
+	
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanza = ensenanza;
+		this.interpretacion = interpretacion;
+	}
+	
+	public String getEsenanza() {
+		return ensenanza;
+	}
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	public int palabrasTotales(int ptotales) {
+		return ptotales;
+	}
+	
+	public String interpretacion() {
+		return interpretacion;
+	}
+	
+	public String toString() {
+		return "holis";
+	}
+}
